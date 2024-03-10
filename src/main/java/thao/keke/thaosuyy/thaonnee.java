a7f4a420061517cc71a17fe6e1051cc4f2881134
thaonee122 <dtc20h4801030023@ictu.edu.vn>
2024-03-10 23:01:23 +0700

    first commit

---
+++ src/main/java/thao/keke/thaosuyy/thaonnee.java
@@ -0,0 +1,10 @@
+package thao.keke.thaosuyy;
+
+public class thaonnee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Cai du co ma");
+	}
+
+}
